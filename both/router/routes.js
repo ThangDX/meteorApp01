2b0e9ff7d1acc6903a1b9e42035999f53fcf98c8
Thang Dang <dangxuanthang@gmail.com>
2015-03-05 16:07:12 +0700

    clip 05

--- both/router/routes.js
+++ both/router/routes.js
@@ -1,5 +1,15 @@
+Router.configure({
+    layoutTemplate: 'appLayout'
+});
+
 Router.route('/', {
   name: 'home'
 });
 
+
 Router.route('/dashboard');
+Router.route('/customers');
+Router.route('/archives');
+Router.route('/roles');
+
+
