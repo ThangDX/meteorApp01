8f92e5e1f8d67661a8b1d81524ed77a6be7a421d
Thang Dang <dangxuanthang@gmail.com>
2015-03-08 18:55:16 +0700

    Calendar Template

--- client/templates/calendar/calendar.js
+++ client/templates/calendar/calendar.js
@@ -2,18 +2,47 @@
  * Created by thangdx on 07/03/2015.
  */
 Template.calendar.rendered = function() {
-    $calendar = $('#projectCalendar');
-    var calendar = $calendar.fullCalendar({
+    var calendar = $('#projectCalendar').fullCalendar({
+        dayClick: function (date, allDay, jsEvent, view) {
+            var ce = {};
+            ce.start = date;
+            ce.end = date;
+            ce.color = 'red';
+            ce.className = 'todo';
+            ce.project = Session.get('active_project');
+            ce.title = 'New Milestone';
+            ce.owner = Meteor.userId();
+            Meteor.call('addCalEvent',ce);
+        },
+        eventClick: function (calEvent, jsEvent,view) {
+            Session.set('editing.calevent',calEvent.id);
+        },
+        eventDrop: function (reqEvent) {
+            Meteor.call('updateEventTimes', reqEvent);
+        },
+        events: function (start, end, callback) {
+            var calEvents = Calevents.find({project: Session.get('active_project')}, {reactive: false}).fetch();
+            callback(calEvents);
+        },
+        eventRender: function (evt, ele) {
+
+        },
        header: {
            left: 'title',
            center: 'today',
            right: 'prev, next'
        },
-        contentHeight: 200,
+        contentHeight: 'auto',
         theme: false,
-        defaultView: 'basicWeek',
+        defaultView: 'month',
         selectable: true,
         editable: true,
         weekMode: 'liquid'
-    });
+    }).data().fullCalendar;
+    Deps.autorun(function () {
+        Calevents.find({project: Session.get('active_project')}).fetch();
+        if(calendar) {
+            calendar.refetchEvents();
+        }
+    })
 }
